af8aaf893c85a8dac6dcee827070f4a7bb794bdf
Duane Bester <duane.bester@sysco.com>
2021-01-18 12:02:18 -0600

    Adding HMAC auth

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 const helmet = require('helmet');
 const compression = require('compression');
 const rateLimit = require("express-rate-limit");
+const crypto = require('crypto');
+const HMAC_KEY = 'cupcakes';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
@@ -40,14 +42,34 @@ app.use(compression());
 app.use(express.json());
 app.use(limiter);
 
+app.use((req, res, next) => {
+    let key = req.query.key;
+    if(!key || key !== '12345') {
+        res.status(403).send();
+        return;
+    }
+    next();
+});
+
 app.get('/data', async (req, res) => {
-    const allData = await SensorData.findAll();
+    let limit = req.query.limit || 5;
+    let offset = req.query.offset || 0;
+    const allData = await SensorData.findAll({ limit, offset });
     res.status(200).send(allData);
     return;
 });
 
 app.post('/data', async (req, res) => {
     let data = req.body;
+    let hmac = req.headers['hmac'];
+    let hmacExpected = crypto.createHmac('sha1', HMAC_KEY)
+        .update(JSON.stringify(data))
+        .digest('hex');
+    let hmacEqual = crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(hmacExpected));
+    if(!hmacEqual) {
+        res.status(403).send('Bad HMAC');
+        return;
+    }
     const sensorData = await SensorData.create(data);
     res.status(201).send(sensorData);
     return;
